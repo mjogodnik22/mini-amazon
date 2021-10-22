7c8c7edeec80ad9019aa94646eff296fd62f41a3
Matthew Jogodnik <mij5@duke.edu>
2021-10-21 21:05:53 -0400

    fixed registration issues, added myaccount and updatebalance pages that are fully functional, added balance to create.sql since it was missing with default of 0, added default to address to prevent nulls, but we might just want to add that to reg form

---
+++ app/myaccount.py
@@ -0,0 +1,20 @@
+from flask import render_template, redirect, url_for, flash, request
+from werkzeug.urls import url_parse
+from flask_login import login_user, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from flask_babel import _, lazy_gettext as _l
+
+from .models.user import User
+
+
+from flask import Blueprint
+bp = Blueprint('myaccount', __name__)
+
+@bp.route('/myAccountPage')
+def myAccountPage():
+#     balance = 0
+#     if current_user.is_authenticated:
+#         balance = User.get_balance(current_user.id)
+    return render_template('myaccount.html', title='My Account')
