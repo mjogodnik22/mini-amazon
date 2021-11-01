bb2a42862a27aa9648bb27162bab0882254bc076
Matthew Jogodnik <mij5@duke.edu>
2021-11-01 19:53:01 -0400

    fixed pid and oid starting from 1

--- db/load.sql
+++ db/load.sql
@@ -5,7 +5,13 @@ SELECT pg_catalog.setval('public.users_id_seq',
 \COPY Sellers FROM 'data/Sellers.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Category FROM 'data/Categories.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Products FROM 'data/Products.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.products_pid_seq',
+                         (SELECT MAX(pid)+1 FROM Products),
+                         false);
 \COPY OrderInformation FROM 'data/OrderInformation.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.orderinformation_oid_seq',
+                         (SELECT MAX(oid)+1 FROM OrderInformation),
+                         false);
 \COPY ItemsInOrder FROM 'data/ItemsInOrder.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY ProductReview FROM 'data/ProductReview.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY SellerReview FROM 'data/SellerReview.csv' WITH DELIMITER ',' NULL '' CSV
