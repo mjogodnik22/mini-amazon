580ceb94174986d1e34abac8a6a9f0e60df3567f
ben brutocao <bdb47@duke.edu>
2021-11-14 15:45:39 -0500

    increased functionality

--- app/RemoveFromCart.py
+++ app/RemoveFromCart.py
@@ -18,18 +18,26 @@ from flask import Blueprint
 bp = Blueprint('removeFromCart', __name__)
 
 class UpdateCart(FlaskForm):
-    confirm = SelectField(_l('Please Confirm'), choices = [(1,"I do not confirm"),(2,"I confirm")],validators=[DataRequired()])
-    submit = SubmitField(_l('Remove From Cart'))
+    confirm = SelectField(_l('You want more? Fewer? None at all?'), choices = [(1,"Add"),(2,"Subtract"),(3,"Delete")],validators=[DataRequired()])
+    amount = StringField(_l('Please Enter the magnitude of change (Not Necessary for Deletion.'))
+    submit = SubmitField(_l('Make The Adjustment'))
 
 @bp.route('/updateCart/<pid>', methods=['GET', 'POST'])
 def updateCart(pid):
     ido = Cartesian.getspecific(current_user.id,pid)
     product = ProductSummary.get(pid)
     form12 = UpdateCart()
+    addorsub = False
     if form12.validate_on_submit:
+        if form12.confirm.data == '1':
+            Cartesian.addToCartAgain(current_user.id,pid,int(form12.amount.data),product.price)
+            return redirect(url_for('Cart.myCart', uid = current_user.id))
         if form12.confirm.data == '2':
+            Cartesian.subFromCart(current_user.id,pid,int(form12.amount.data),product.price)
+            return redirect(url_for('Cart.myCart', uid = current_user.id))
+        if form12.confirm.data == '3':
+            print(form12.amount.data)
             Cartesian.removeFromCart(pid,current_user.id)
-            flash("Successfully removed from Cart!")
             return redirect(url_for('Cart.myCart', uid = current_user.id))
 
     return render_template('removeFromCart.html', title='Update Cart', form=form12, prod = product,currcart = ido)
