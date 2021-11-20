b80d06201efebef52514c1afce2da664a36409fc
Matthew Jogodnik <mij5@duke.edu>
2021-11-20 12:36:16 -0500

    unspecified changes

--- app/productPage.py
+++ app/productPage.py
@@ -15,19 +15,14 @@ from flask import Blueprint
 bp = Blueprint('productPage', __name__)
 
 class NewFilterForm(FlaskForm):
-    category = SelectField(_l('Filter by Category'), validators=[DataRequired()])
-    submit = SubmitField(_l('Submit'))
-    
-class NewSortForm(FlaskForm):
-    key = SelectField(_l('Sort by Key'), choices = [('name','Name'),('pid','ID'),('price','Price'),('pid','Clear Sort')],validators=[DataRequired()])
+    category = SelectField(_l('Filter by Category'))
+    key = SelectField(_l('Sort by Key'), choices = [('name','Name'),('pid','ID'),('price','Price'),('pid','Clear Sort')])
+    page_val = IntegerField(_l('Jump to Page'))
+    search_val = StringField(_l('Search:'))
     submit = SubmitField(_l('Submit'))
 
 class NewPageForm(FlaskForm):
     page_val = IntegerField(_l('Jump to Page'),validators=[DataRequired()])
-    submit = SubmitField(_l('Submit'))
-
-class NewSearchForm(FlaskForm):
-    search_val = StringField(_l('Search:'),validators=[DataRequired()])
     submit = SubmitField(_l('Submit'))
 #looking into a better way of implementing
 #weird stuff with none that I will remove the hardcode for when I figure it out
@@ -44,32 +39,37 @@ def productPage(page_num = 1, sort_by = 'pid',filter_by = 'None'):
     products = Product.get_all_page(page_num = page_num, sort_by = sort_by, filter_by = filter_by)
     filterform = NewFilterForm()
     filter_categories = Product.get_categories()
-    filter_categories.append((None, 'Clear Filter'))
+    filter_categories.append((None, 'Select', 'Clear Filter'))
     filterform.category.choices = filter_categories
-    sortform = NewSortForm()
-    pageform = NewPageForm()
-    searchform = NewSearchForm()
     prev_page = page_num - 1
     #only need to compute page count on addition of a product, will look into this
     next_page = page_num + 1
     max_pages = Product.get_page_count()
-    
-    if next_page > max_pages:
-        next_page = 0
     if filterform.validate_on_submit():
+        print(filterform.search_val.data)
+        if next_page > max_pages:
+            next_page = 0
+        if filterform.category.data != "":
+            filter_by = filterform.category.data
+        if filterform.key.data != "":
+            sort_by = filterform.key.data
+        if filterform.page_val.data != "":
+            page_num = filterform.page_val.data
+        if filterform.search_val.data != "":
+            return redirect(url_for('productPage.productSearchPage', page_num = page_num, query = filterform.search_val.data))
         return redirect(url_for('productPage.productPage', page_num = page_num, sort_by = sort_by, filter_by = filterform.category.data))
-    if sortform.validate_on_submit():
-        return redirect(url_for('productPage.productPage', page_num = page_num, sort_by = sortform.key.data, filter_by = filter_by))
-    if pageform.validate_on_submit():
-        if pageform.page_val.data < 1 or pageform.page_val.data > max_pages:
-            flash('Please input a page number between 1 and {num1}!'.format(num1 = max_pages))
-            return redirect(url_for('productPage.productPage', page_num = page_num, sort_by = sort_by, filter_by = filter_by))
-        else:
-            return redirect(url_for('productPage.productPage', page_num = pageform.page_val.data, sort_by = sort_by, filter_by = filter_by))
-    if searchform.validate_on_submit():
-        return redirect(url_for('productPage.productSearchPage', page_num = page_num, query = searchform.search_val.data))
+        # if sortform.validate_on_submit():
+        #     return redirect(url_for('productPage.productPage', page_num = page_num, sort_by = sortform.key.data, filter_by = filter_by))
+        # if pageform.validate_on_submit():
+        #     if pageform.page_val.data < 1 or pageform.page_val.data > max_pages:
+        #         flash('Please input a page number between 1 and {num1}!'.format(num1 = max_pages))
+        #         return redirect(url_for('productPage.productPage', page_num = page_num, sort_by = sort_by, filter_by = filter_by))
+        #     else:
+        #         return redirect(url_for('productPage.productPage', page_num = pageform.page_val.data, sort_by = sort_by, filter_by = filter_by))
+        # if searchform.validate_on_submit():
+        #     return redirect(url_for('productPage.productSearchPage', page_num = page_num, query = searchform.search_val.data))
     
-    return render_template('productpage.html',form1=filterform, form2 = sortform, form3 = pageform,form4 = searchform,
+    return render_template('productpage.html',form1=filterform,
                            avail_products = products, next_page=next_page, prev_page=prev_page, curr_sort = sort_by, curr_filter = filter_by)
 
 @bp.route('/products_search',defaults={'query' :'', 'page_num' : 1},methods=['GET', 'POST'])
